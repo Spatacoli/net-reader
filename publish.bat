219c3386959019b98650329f79d63338a8787ac9
Todd Spatafore <todd.spatafore@vudu.com>
2020-12-21 14:13:03 -0800

    Adding the publish script and the tasks.

--- publish.bat
+++ publish.bat
@@ -1,4 +1,4 @@
 dotnet publish -r linux-arm --self-contained false /p:ShowLinkerSizeComparison=true
 pushd .\bin\Debug\net5.0\linux-arm\publish
-scp -v -r .\* pi@ashley:/home/pi/Projects/test
+scp -v -r .\* pi@ashley:~/Projects/net-reader
 popd
